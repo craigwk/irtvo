a024b411a90cdb317816bedc0886391e26d6c902
peberoftyrkisk <peberoftyrkisk@aea4ba7e-262c-ea91-1a65-e32e6118e7c5>
2011-03-02 14:58:44 +0000

    iRTVO 1.0.1 - fixes couple ticker bugs - main.png is shown the topmost - added "leader" translation

---
+++ tags/1.0.1/iRTVO/shared.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * shared.cs
+ * 
+ * SharedData class:
+ * 
+ * Holds the data structures which are shared between API and overlay.
+ * 
+ * API uses mutexes while writing to the DriverInfo, LapInfo, SessionInfo and TrackInfo structures.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// additional
+using System.Threading;
+
+namespace iRTVO
+{
+    class SharedData
+    {
+        public struct DriverInfo
+        {
+            public string name;
+            public string initials;
+            public string shortname;
+
+            public string club;
+            public string car;
+            public int carclass;
+            public string license;
+
+            public float fastestlap;
+            public float previouslap;
+            public int completedlaps;
+            public DateTime lastNewLap;
+            public int lastNewLapNr;
+
+            public int userId;
+            public int carId;
+            public int numberPlate;
+
+            public Boolean onTrack;
+            public DateTime offTrackSince;
+
+        }
+
+        public struct LapInfo
+        {
+            public int id;
+            public float diff;
+            public int lapDiff;
+            public float completedLaps;
+            public float fastLap;
+        }
+
+        public struct SessionInfo
+        {
+            public int laps;
+            public int lapsRemaining;
+
+            public float time;
+            public float timeRemaining;
+
+            public iRacingTelem.eSessionType type;
+            public iRacingTelem.eSessionState state;
+            public iRacingTelem.eSessionFlag flag;
+
+            public int driverFollowed;
+        }
+
+        public struct TrackInfo
+        {
+            public string name;
+            public int id;
+            public float length;
+        }
+
+        public enum overlayObjects
+        {
+            driver,
+            sidepanel,
+            results,
+            replay,
+            sessionstate,
+            flag,
+            startlights,
+            ticker,
+            laptime
+        }
+
+        public enum sidepanelTypes
+        {
+            leader,
+            followed,
+            fastlap
+        }
+
+        public enum ConnectionState
+        {
+            initializing = 0,
+            connecting,
+            active,
+        }
+
+        // DriverInfo
+        public static Mutex driversMutex;
+        public static DriverInfo[] drivers = new DriverInfo[iRacingTelem.MAX_CARS];
+
+        // LapInfo
+        public static Mutex standingMutex;
+        public static LapInfo[][] standing = new LapInfo[iRacingTelem.MAX_SESSIONS][];
+
+        // SessionInfo
+        public static Mutex sessionsMutex;
+        public static SessionInfo[] sessions = new SessionInfo[iRacingTelem.MAX_SESSIONS];
+        public static int currentSession;
+
+        // TrackInfo
+        public static Mutex trackMutex;
+        public static TrackInfo track = new TrackInfo();
+
+        // Overlay states
+        public static Boolean[] visible = new Boolean[Enum.GetValues(typeof(overlayObjects)).Length];
+        public static sidepanelTypes sidepanelType = sidepanelTypes.leader;
+        public static Boolean requestRefresh = false;
+
+        // Result state
+        public static int resultPage = -1;
+        public static Boolean resultLastPage = false;
+        public static int resultSession = -1;
+
+        // API state
+        public static Boolean runApi = true;
+        public static Boolean runOverlay = false;
+        public static ConnectionState apiState;
+
+        // Start light timer
+        public static DateTime startlights;
+
+        // Overlay performance timers
+        public static TimeSpan overlayEffectiveFPS;
+        public static TimeSpan overlayFPS;
+    }
+}
