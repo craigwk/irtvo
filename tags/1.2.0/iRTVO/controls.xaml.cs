de574d97d248cb164ca2ef9de5bc0d78a06fa0d2
peberoftyrkisk <peberoftyrkisk@aea4ba7e-262c-ea91-1a65-e32e6118e7c5>
2011-08-31 19:27:55 +0000

    Tagging 1.2.0

---
+++ tags/1.2.0/iRTVO/controls.xaml.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+using System.Globalization;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace iRTVO
+{
+    /// <summary>
+    /// Interaction logic for controls.xaml
+    /// </summary>
+    public partial class Controls : Window
+    {
+        iRacingAPI API;
+        DateTime cameraUpdate = DateTime.Now;
+        DispatcherTimer updateTimer = new DispatcherTimer();
+        Boolean autoCommitEnabled = false;
+        Thread replayThread;
+
+        public Controls()
+        {
+            InitializeComponent();
+
+            this.Left = Properties.Settings.Default.controlsWindowLocationX;
+            this.Top = Properties.Settings.Default.controlsWindowLocationY;
+
+            if (Properties.Settings.Default.AoTcontrols == true)
+                this.Topmost = true;
+            else
+                this.Topmost = false;
+
+            API = new iRTVO.iRacingAPI();
+            API.sdk.Startup();
+        }
+
+        // no focus
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            //Set the window style to noactivate.
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SetWindowLong(helper.Handle, GWL_EXSTYLE, GetWindowLong(helper.Handle, GWL_EXSTYLE) | WS_EX_NOACTIVATE);
+        }
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+
+        private void controlsWindow_LocationChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.controlsWindowLocationX = (int)this.Left;
+            Properties.Settings.Default.controlsWindowLocationY = (int)this.Top;
+            Properties.Settings.Default.Save();
+        }
+
+        private void controlsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            updateTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            updateTimer.Tick += new EventHandler(updateControls);
+            updateTimer.Start();
+            updateControls(new object(), new EventArgs());
+            
+        }
+
+        private void updateControls(object sender, EventArgs e)
+        {
+            if (SharedData.Camera.Updated > cameraUpdate)
+            {
+                if (SharedData.Camera.Groups.Count > 0)
+                {
+                    cameraSelectComboBox.Items.Clear();
+                    ComboBoxItem cboxitem;
+
+                    foreach (CameraInfo.CameraGroup cam in SharedData.Camera.Groups)
+                    {
+                        cboxitem = new ComboBoxItem();
+                        cboxitem.Content = cam.Id + " " + cam.Name;
+                        cameraSelectComboBox.Items.Add(cboxitem);
+                        if (cam.Id == SharedData.Camera.CurrentGroup)
+                            cameraSelectComboBox.SelectedItem = cboxitem;
+                    }
+
+                    cboxitem = new ComboBoxItem();
+                    cboxitem.Content = "Most exiting";
+                    cameraSelectComboBox.Items.Add(cboxitem);
+
+                    cameraUpdate = DateTime.Now;
+                }
+            }
+
+            if ((SharedData.Drivers.Count - 2) > driverSelect.Items.Count || 
+                (driverSelect.SelectedItem != null && SharedData.updateControls))
+            {
+                driverSelect.Items.Clear();
+                ComboBoxItem cboxitem;
+
+                foreach (DriverInfo driver in SharedData.Drivers)
+                {
+                    if (driver.Name != "Pace Car" && driver.CarIdx < 63)
+                    {
+                        cboxitem = new ComboBoxItem();
+                        cboxitem.Content = driver.NumberPlate + " " + driver.Name;
+                        driverSelect.Items.Add(cboxitem);
+                        if(driver.CarIdx == SharedData.Sessions.CurrentSession.FollowedDriver.Driver.CarIdx)
+                            driverSelect.SelectedItem = cboxitem;
+                    }
+                }
+                SharedData.updateControls = false;
+            }
+
+        }
+
+        private void autoCommit(object sender, SelectionChangedEventArgs e)
+        {
+            if (autoCommitEnabled)
+                commit();
+        }
+
+        private void commit()
+        {
+            if (driverSelect.SelectedItem != null && cameraSelectComboBox.SelectedItem != null)
+            {
+                String[] split = cameraSelectComboBox.SelectedItem.ToString().Split(' ');
+                if (split[1] == "Most")
+                {
+                    API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.CamSetState, (int)iRSDKSharp.CameraStateTypes.UIHidden, 0);
+                }
+                else
+                {
+                    int camera = Int32.Parse(split[1]);
+                    split = driverSelect.SelectedItem.ToString().Split(' ');
+                    int driver = Int32.Parse(split[1]);
+                    API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.CamSwitchNum, driver, camera);
+                }
+                
+            }
+        }
+
+        private void commitButton_Click(object sender, RoutedEventArgs e)
+        {
+            commit();
+        }
+
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (API.sdk.IsConnected())
+            {
+                Int32 playspeed = (Int32)API.sdk.GetData("ReplayPlaySpeed");
+                if (playspeed > 0)
+                {
+                    API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 0, 0);
+                }
+                else
+                {
+                    API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 1, 0);
+                }
+            }
+        }
+
+        private void liveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (API.sdk.IsConnected())
+            {
+                /*
+                API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySearch, (int)iRSDKSharp.ReplaySearchModeTypes.ToEnd, 0);
+                API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 1, 0);
+                 * */
+                replayThread = new Thread(live);
+                replayThread.Start();
+            }
+        }
+
+        private void addBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            if (SharedData.Sessions.CurrentSession.FollowedDriver.Driver.NumberPlate.Length > 0)
+            {
+                Event ev = new Event(Event.eventType.fastlap, (Int32)API.sdk.GetData("ReplayFrameNum"), SharedData.Sessions.CurrentSession.FollowedDriver.Driver, "Manual bookmark", SharedData.Sessions.CurrentSession.Type, SharedData.Sessions.CurrentSession.FollowedDriver.CurrentLap.LapNum);
+                SharedData.Bookmarks.List.Add(ev);
+            }
+        }
+
+        private void beginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Event prevEvent;
+            int diff = 0;
+
+            if(SharedData.Bookmarks.List.Count > 0) 
+            {
+                prevEvent = SharedData.Bookmarks.List[0]; // pick first
+                diff = (Int32)API.sdk.GetData("ReplayFrameNum");
+
+                foreach (Event ev in SharedData.Bookmarks.List)
+                {
+                    if (((Int32)API.sdk.GetData("ReplayFrameNum") - ev.ReplayPos) < diff)
+                    {
+                        prevEvent = ev;
+                    }
+                }
+
+                replayThread = new Thread(rewind);
+                replayThread.Start(prevEvent);
+                /*
+                API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.CamSwitchNum, Int32.Parse(prevEvent.Driver.NumberPlate), -1);
+                API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlayPosition, 0, prevEvent.ReplayPos);
+                API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 1, 0);
+                 * */
+            }
+        }
+
+        private void autoCommitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)e.Source;
+
+            if (autoCommitEnabled)
+            {
+                btn.Content = "Auto apply";
+                autoCommitEnabled = false;
+            }
+            else
+            {
+                btn.Content = "Manual apply";
+                autoCommitEnabled = true;
+            }
+        }
+
+        public void rewind(Object input)
+        {
+            Event ev = (Event)input;
+
+            SharedData.replayInProgress = true;
+            SharedData.replayReady.Reset();
+            SharedData.replayReady.WaitOne();
+
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 0, 0);
+
+            Thread.Sleep(Properties.Settings.Default.ReplayMinLength - 200);
+
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.CamSwitchNum, Int32.Parse(ev.Driver.NumberPlate), -1);
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlayPosition, 0, ev.ReplayPos);
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 1, 0);
+
+            SharedData.updateControls = true;
+
+            SharedData.replayInProgress = false;
+        }
+
+        public void live()
+        {
+
+            SharedData.replayInProgress = true;
+            SharedData.replayReady.Reset();
+            SharedData.replayReady.WaitOne();
+
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 0, 0);
+
+            Thread.Sleep(Properties.Settings.Default.ReplayMinLength - 200);
+
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySearch, (int)iRSDKSharp.ReplaySearchModeTypes.ToEnd, 0);
+            API.sdk.BroadcastMessage(iRSDKSharp.BroadcastMessageTypes.ReplaySetPlaySpeed, 1, 0);
+
+            SharedData.updateControls = true;
+
+            SharedData.replayInProgress = false;
+        }
+
+        private void instantReplay_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem cbi = (ComboBoxItem)Rewind.SelectedItem;
+            string secstr = cbi.Content.ToString();
+            int secint = Int32.Parse(secstr.Substring(0, secstr.Length - 1));
+
+            Event ev = new Event(
+                Event.eventType.bookmark,
+                (Int32)API.sdk.GetData("ReplayFrameNum") - (secint * 60),
+                SharedData.Sessions.CurrentSession.FollowedDriver.Driver,
+                "",
+                Sessions.SessionInfo.sessionType.invalid,
+                0);
+
+            replayThread = new Thread(rewind);
+            replayThread.Start(ev);
+        }
+    }
+}
---
+++ tags/1.2.0/iRTVO/webTiming.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// additional
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace iRTVO
+{
+    class webTiming
+    {
+        //webTimingObject data;
+
+        struct webtimingDriver
+        {
+            public string position;
+            public string name;
+            public string number;
+            public string lap;
+            public string fastestlap;
+            public string previouslap;
+            public string interval;
+            public string gap;
+            public string[] sectors;
+            public string pit;
+            public string lapsled;
+            public bool retired;
+
+            public webtimingDriver(Sessions.SessionInfo.StandingsItem driver)
+            {
+                position = driver.Position.ToString();
+                name = driver.Driver.Name;
+                number = driver.Driver.NumberPlate;
+                lap = driver.CurrentLap.LapNum.ToString();
+                fastestlap = iRTVO.Overlay.floatTime2String(driver.FastestLap, 3, false);
+                previouslap = iRTVO.Overlay.floatTime2String(driver.PreviousLap.LapTime, 3, false);
+                pit = driver.PitStops.ToString();
+                lapsled = driver.LapsLed.ToString();
+
+                Sessions.SessionInfo.StandingsItem leader = SharedData.Sessions.CurrentSession.FindPosition(1);
+                Sessions.SessionInfo.StandingsItem infront;
+
+                if(driver.Position <= 1) {
+                    infront = new Sessions.SessionInfo.StandingsItem();
+                }
+                else {
+                    infront = SharedData.Sessions.CurrentSession.FindPosition(driver.Position - 1);
+                }
+
+                if (SharedData.Sessions.CurrentSession.Type == Sessions.SessionInfo.sessionType.race
+                    /* && (driver.Finished == true || driver.Sector == 0) */)
+                {
+                    if (infront.PreviousLap.LapNum > driver.PreviousLap.LapNum)
+                    {
+                        interval = (infront.FindLap(driver.PreviousLap.LapNum).LapNum - driver.PreviousLap.LapNum) + " L";
+                    }
+                    else
+                    {
+                        interval = iRTVO.Overlay.floatTime2String((driver.PreviousLap.Gap - infront.FindLap(driver.PreviousLap.LapNum).Gap), 3, false);
+                    }
+
+                    if (driver.PreviousLap.GapLaps > 0)
+                    {
+                        gap = driver.PreviousLap.GapLaps +" L";
+                    }
+                    else
+                    {
+                        gap = iRTVO.Overlay.floatTime2String(driver.PreviousLap.Gap, 3, false);
+                    }
+                }
+                /*
+                else if (SharedData.Sessions.CurrentSession.Type == Sessions.SessionInfo.sessionType.race &&
+                    driver.Finished == false)
+                {
+
+                    if (infront.PreviousLap.LapNum > driver.PreviousLap.LapNum && infront.CurrentTrackPct - driver.CurrentTrackPct > 1)
+                    {
+                        interval = (infront.PreviousLap.LapNum - driver.PreviousLap.LapNum) + " L";
+                    }
+                    else
+                    {
+                        /*
+                        if (SharedData.Sectors.Count > 0 && infront.FindLap(driver.CurrentLap.LapNum).SectorTimes.Count > 0)
+                        {
+                            DateTime infrontsector = infront.FindLap(driver.CurrentLap.LapNum).SectorTimes.Find(s => s.Num.Equals(driver.Sector - 1)).Begin;
+                            DateTime mysector = driver.CurrentLap.SectorTimes.Find(s => s.Num.Equals(driver.Sector - 1)).Begin;
+                            interval = iRTVO.Overlay.floatTime2String((float)(infrontsector - mysector).TotalSeconds, 3, false);
+                        }
+                        else
+                        {
+                        
+                            interval = iRTVO.Overlay.floatTime2String((infront.PreviousLap.Gap - driver.PreviousLap.Gap), 3, false);
+                        //}
+                       
+                    }
+
+                    if (driver.PreviousLap.GapLaps > 0)
+                    {
+                        gap = driver.PreviousLap.GapLaps + " L";
+                    }
+                    else
+                    {
+
+                        /*if (leader.FindLap(driver.CurrentLap.LapNum).SectorTimes.Count > 0)
+                        {
+                            DateTime leadersector = leader.FindLap(driver.CurrentLap.LapNum).SectorTimes.Find(s => s.Num.Equals(driver.Sector - 1)).Begin;
+                            DateTime mysector = driver.CurrentLap.SectorTimes.Find(s => s.Num.Equals(driver.Sector - 1)).Begin;
+                            //gap = iRTVO.Overlay.floatTime2String(driver.PreviousLap.Gap, 3, false);
+                            gap = iRTVO.Overlay.floatTime2String((float)(leadersector - mysector).TotalSeconds, 3, false);
+                        }
+                        else
+                        {
+                        
+                            gap = gap = iRTVO.Overlay.floatTime2String(driver.PreviousLap.Gap, 3, false);
+                        //}
+                    }
+                }
+                */
+                else
+                {
+                    interval = iRTVO.Overlay.floatTime2String((driver.FastestLap - infront.FastestLap), 3, false);
+                    gap = iRTVO.Overlay.floatTime2String((driver.FastestLap - leader.FastestLap), 3, false);
+                }
+
+                sectors = new string[SharedData.SelectedSectors.Count];
+
+                if (SharedData.SelectedSectors.Count > 0)
+                {
+                    
+                    for (int i = 0; i < SharedData.SelectedSectors.Count; i++)
+                    {
+                        if (driver.Sector <= 0) // first sector, show previous lap times
+                        {
+                            if (i < driver.PreviousLap.SectorTimes.Count)
+                            {
+                                sectors[i] = iRTVO.Overlay.floatTime2String(driver.PreviousLap.SectorTimes.First(s => s.Num.Equals(i)).Time, 1, false);
+                            }
+                            else
+                            {
+                                sectors[i] = "-.--";
+                            }
+                        }
+                        else
+                        {
+                            if (i < driver.CurrentLap.SectorTimes.Count)
+                            {
+                                sectors[i] = iRTVO.Overlay.floatTime2String(driver.CurrentLap.SectorTimes.First(s => s.Num.Equals(i)).Time, 1, false);
+                            }
+                            else
+                            {
+                                sectors[i] = "-.--";
+                            }
+                        }
+                    }
+                }
+
+                if (SharedData.Sessions.CurrentSession.Type == Sessions.SessionInfo.sessionType.race &&
+                    driver.TrackSurface == Sessions.SessionInfo.StandingsItem.SurfaceType.NotInWorld && 
+                SharedData.allowRetire && 
+                (DateTime.Now - driver.OffTrackSince).TotalSeconds > 1)
+                {
+                    retired = true;
+                    if (infront.CurrentLap.LapNum > driver.CurrentLap.LapNum)
+                    {
+                        interval = (infront.CurrentLap.LapNum - driver.CurrentLap.LapNum) + " L";
+                    }
+                    else
+                    {
+                        interval = iRTVO.Overlay.floatTime2String((driver.PreviousLap.Gap - infront.PreviousLap.Gap), 3, false);
+                    }
+                    if ((leader.CurrentLap.LapNum - driver.CurrentLap.LapNum) > 0)
+                    {
+                        gap = leader.CurrentLap.LapNum - driver.CurrentLap.LapNum + " L";
+                    }
+                    else
+                    {
+                        gap = iRTVO.Overlay.floatTime2String(driver.PreviousLap.Gap, 3, false);
+                    }
+                }
+                else
+                {
+                    retired = false;
+                }
+            }
+        }
+
+        struct webTimingObject
+        {
+            public string trackname;
+            public string sessiontype;
+            public string sessionstate;
+            public float timeremaining;
+            public int currentlap;
+            public int totallaps;
+            public string sessionflag;
+            public int cautions;
+            public int cautionlaps;
+
+            public webtimingDriver[] drivers;
+        }
+        
+        private string postURL;
+
+        public webTiming(string url) {
+            postURL = url;
+
+        }
+
+        public void postData(object o)
+        {
+            // wait
+            SharedData.writeMutex.WaitOne(1000);
+            SharedData.readMutex.WaitOne(1000);
+
+            webTimingObject data = new webTimingObject();
+
+            data.trackname = SharedData.Track.name;
+            data.sessiontype = SharedData.Sessions.CurrentSession.Type.ToString();
+            data.sessionstate = SharedData.Sessions.CurrentSession.State.ToString();
+
+            if (SharedData.Sessions.CurrentSession.State == Sessions.SessionInfo.sessionState.checkered ||
+                SharedData.Sessions.CurrentSession.State == Sessions.SessionInfo.sessionState.cooldown)
+            {
+                data.sessionflag = Sessions.SessionInfo.sessionFlag.checkered.ToString();
+            }
+            else 
+            {
+                data.sessionflag = SharedData.Sessions.CurrentSession.Flag.ToString();
+            }
+
+            data.currentlap = SharedData.Sessions.CurrentSession.LapsComplete;
+            data.totallaps = SharedData.Sessions.CurrentSession.LapsTotal;
+            data.timeremaining = (float)SharedData.Sessions.CurrentSession.TimeRemaining;
+            data.cautions = SharedData.Sessions.CurrentSession.Cautions;
+            data.cautionlaps = SharedData.Sessions.CurrentSession.CautionLaps;
+
+            data.drivers = new webtimingDriver[SharedData.Sessions.CurrentSession.Standings.Count];
+
+            IEnumerable<Sessions.SessionInfo.StandingsItem> query = SharedData.Sessions.CurrentSession.Standings.OrderBy(s => s.Position);
+
+            int i = 0;
+            foreach (Sessions.SessionInfo.StandingsItem si in query)
+            {
+                data.drivers[i] = new webtimingDriver(si);
+                i++;
+            }
+
+            send(JsonConvert.SerializeObject(data));
+        }
+
+
+        /// <summary>
+        /// method for validating a url with regular expressions
+        /// </summary>
+        /// <param name="url">url we're validating</param>
+        /// <returns>true if valid, otherwise false</returns>
+        public static bool isValidUrl(ref string url)
+        {
+            string pattern = @"^(http|https|ftp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
+            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return reg.IsMatch(url);
+        }
+
+        public void send(string postData)
+        {
+            if (isValidUrl(ref postURL) && SharedData.webError.Length <= 0)
+            {
+                // Create a request using a URL that can receive a post.
+                WebRequest request = WebRequest.Create(postURL);
+
+                // Set the Method property of the request to POST.
+                request.Method = "POST";
+
+                // Create POST data and convert it to a byte array.
+                byte[] byteArray = Encoding.UTF8.GetBytes("key=" + Properties.Settings.Default.webTimingKey + "&sessionid=" + SharedData.Sessions.SessionId.ToString() + "&subsessionid=" + SharedData.Sessions.SubSessionId.ToString() + "&sessionnum=" + SharedData.Sessions.CurrentSession.Id.ToString() + "&type=" + SharedData.Sessions.CurrentSession.Type.ToString() + "&data=" + postData);
+
+                // Set the ContentType property of the WebRequest.
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                // Set the ContentLength property of the WebRequest.
+                request.ContentLength = byteArray.Length;
+
+                // Get the request stream.
+                Stream dataStream = request.GetRequestStream();
+
+                // Write the data to the request stream.
+                dataStream.Write(byteArray, 0, byteArray.Length);
+
+                SharedData.webBytes += byteArray.Length;
+
+                // Close the Stream object.
+                dataStream.Close();
+
+                // Get the response.
+                WebResponse response;
+                try
+                {
+                    response = request.GetResponse();
+                }
+                catch (WebException ex)
+                {
+                    SharedData.webError += "\n" + ex.Message;
+                    response = ex.Response as HttpWebResponse;
+                }
+
+                // Display the status.
+                //Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+                // Get the stream containing content returned by the server.
+                try
+                {
+                    dataStream = response.GetResponseStream();
+                }
+                catch
+                {
+                    SharedData.webError = "Timeout";
+                }
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+
+                // Display the content.
+                //Console.WriteLine(responseFromServer);
+                if (responseFromServer.Length > 0)
+                {
+                    SharedData.webError += "\n" + responseFromServer;
+                    Console.WriteLine("Error posting: " + responseFromServer);
+                }
+
+                // Clean up the streams.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                if(SharedData.webError.Length > 0)
+                    System.Windows.MessageBox.Show(SharedData.webError);
+            }
+           
+        }
+
+    }
+}
