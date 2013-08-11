71df0a1dcba332c8e3b561d5c2b083423135c0ee
peberoftyrkisk@gmail.com <peberoftyrkisk@gmail.com@aea4ba7e-262c-ea91-1a65-e32e6118e7c5>
2013-08-11 12:49:27 +0000

    Tagging 1.2.12

---
+++ tags/1.2.12/iRTVO/scripting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSScriptLibrary;
+
+namespace iRTVO
+{
+    public interface IHost
+    {
+        iRTVO.Sessions.SessionInfo getSession();
+    }
+
+    public interface IScript
+    {
+        IHost Parent { set; }
+        String init();
+        String DriverInfo(String method, iRTVO.Sessions.SessionInfo.StandingsItem standing, iRTVO.Sessions.SessionInfo session, Int32 rounding);
+        String SessionInfo(String method, iRTVO.Sessions.SessionInfo session, Int32 rounding);
+
+    }
+
+    class Scripting : IHost
+    {
+        Dictionary<String, IScript> scripts = new Dictionary<String, IScript>();
+
+        // interfaces to app
+        public void loadScript(String filename)
+        {
+            IScript sc = CSScript.Evaluator.LoadFile<IScript>(filename);
+            sc.Parent = this;
+            String scname = sc.init();
+            scripts.Add(scname, sc);
+        }
+
+        public String[] getScripts()
+        {
+            return scripts.Keys.ToArray();
+        }
+
+        public String getDriverInfo(String script, String method, Sessions.SessionInfo.StandingsItem standing, Sessions.SessionInfo session, Int32 rounding)
+        {
+            return scripts[script].DriverInfo(method, standing, session, rounding);
+        }
+
+        public String getSessionInfo(String script, String method, Sessions.SessionInfo session, Int32 rounding)
+        {
+            return scripts[script].SessionInfo(method, session, rounding);
+        }
+
+        // interfaces to scripts
+        iRTVO.Sessions.SessionInfo IHost.getSession()
+        {
+            return SharedData.Sessions.CurrentSession;
+        }
+    }
+}
---
+++ tags/1.2.12/iRTVO/scripts/HelloWorld.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Script : iRTVO.IScript
+{
+    public iRTVO.IHost Parent { set; get; }
+
+    public String init()
+    {
+        // returns script name and does other initialization
+        return "helloworld";
+    }
+
+    public String DriverInfo(String method, iRTVO.Sessions.SessionInfo.StandingsItem standing, iRTVO.Sessions.SessionInfo session, Int32 rounding)
+    {
+        switch (method)
+        {
+            case "test":
+                return "test succesful";
+                break;
+            case "drivername":
+                return standing.Driver.Name;
+                break;
+            default:
+                return "[invalid]";
+                break;
+        }
+    }
+
+    public String SessionInfo(String method, iRTVO.Sessions.SessionInfo session, Int32 rounding)
+    {
+        Console.WriteLine(method);
+        switch (method)
+        {
+            case "test":
+                return "test succesful";
+                break;
+            case "state":
+                return session.State.ToString();
+                break;
+            default:
+                return "[invalid]";
+                break;
+        }
+    }
+}
